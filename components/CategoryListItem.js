1c16026fd18c2785f19cf9320c03f053135d9b6e
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2021-01-29 10:52:58 +0700

    add TouchOpacity && Alert when onPress()

--- components/CategoryListItem.js
+++ components/CategoryListItem.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, Text, View, TouchableOpacity, Alert } from 'react-native'
 import styles from './CategoryListItemStyle.js'
 
 export default function CategoryListItem(props) {
   const { category } = props
   return (
-  <View style={ styles.categoryView }>
-    <Text style={ styles.categoryText }> {category.title} </Text>
-    <Image source={category.image} style={styles.categoryImage}></Image>
-  </View>
+  <TouchableOpacity activeOpacity={0.5} onPress={() => 
+    Alert.alert(
+      category.title,
+      `Click OK to show ${category.title}`,
+      [
+        {
+          text: 'OK',
+          onPress: () => console.log('OK Pressed'),
+        },
+        {
+          text: 'Ask me later',
+          onPress: () => console.log('Ask me later pressed'),
+          style: 'destructive'
+        },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel'
+        }
+      ],
+      { cancelable: true }
+    )
+  }>
+    <View style={ styles.categoryView }>
+      <Text style={ styles.categoryText }> {category.title} </Text>
+      <Image source={category.image} style={styles.categoryImage}></Image>
+    </View>
+  </TouchableOpacity>
   )
 }
